5653baf881fd8d0f948aedee427cbd15c2e9cd6f
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-23 12:34:16 +0200

    Fattura Repository e Service
    Creazione repository e service dell'entità fattura con query personalizzate, come richiesto dalla traccia

---
+++ src/main/java/Epicode/epicenergy/services/FatturaService.java
@@ -0,0 +1,53 @@
+package Epicode.epicenergy.services;
+
+import Epicode.epicenergy.entities.Fattura;
+import Epicode.epicenergy.enums.StatoFattura;
+import Epicode.epicenergy.repositories.FatturaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class FatturaService {
+
+    @Autowired
+    private FatturaRepository fatturaRepository;
+
+
+    public Fattura saveFattura(Fattura fattura) {
+        return fatturaRepository.save(fattura);
+    }
+
+
+    public List<Fattura> getFattureByClienteId(Long clienteId) {
+        return fatturaRepository.findByClienteId(clienteId);
+    }
+
+
+    public List<Fattura> getFattureByStato(StatoFattura stato) {
+        return fatturaRepository.findByStato(stato);
+    }
+
+
+    public List<Fattura> getFattureByDateRange(LocalDate startDate, LocalDate endDate) {
+        return fatturaRepository.findByDataBetween(startDate, endDate);
+    }
+
+
+    public List<Fattura> getFattureByAnno(Integer anno) {
+        return fatturaRepository.findByAnno(anno);
+    }
+
+
+    public List<Fattura> getFattureByImportoRange(BigDecimal minImporto, BigDecimal maxImporto) {
+        return fatturaRepository.findByImportoBetween(minImporto, maxImporto);
+    }
+
+
+    public List<Fattura> getFattureFiltrate(Long clienteId, StatoFattura stato, BigDecimal minImporto, BigDecimal maxImporto, LocalDate startDate, LocalDate endDate) {
+        return fatturaRepository.findByClienteIdAndStatoAndImportoBetweenAndDataBetween(clienteId, stato, minImporto, maxImporto, startDate, endDate);
+    }
+}
