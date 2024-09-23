fa1a98257a91a6796b28c64ecd074f1ad97c3a73
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-23 12:23:43 +0200

    aggiunti file CSV

--- src/main/java/Epicode/epicenergy/tools/ImportCSV.java
+++ src/main/java/Epicode/epicenergy/tools/ImportCSV.java
@@ -1,5 +1,17 @@
 package Epicode.epicenergy.tools;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class ImportCSV {
-    public void importCSV
+    public void importComuni(){
+        String filePath= new File("./").getAbsolutePath();
+        boolean isFirstLine=true;
+
+        try(BufferedReader bufferedReader=new BufferedReader(new FileReader(filePath)))) {
+            String line;
+
+        }
+    }
 }
