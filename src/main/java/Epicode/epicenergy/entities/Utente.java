23ff507d6b152b984572aa025548c29a86114d12
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-23 12:10:11 +0200

    Merge branch 'develop' of https://github.com/Edyan90/epicenergy into develop

---
+++ src/main/java/Epicode/epicenergy/entities/Utente.java
@@ -0,0 +1,41 @@
+package Epicode.epicenergy.entities;
+
+import Epicode.epicenergy.enums.Ruolo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@JsonIgnoreProperties
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private int id;
+
+    private String username;
+    private String mail;
+    private String password;
+    private String nome;
+    private String cognome;
+    private String avatar;
+    @Enumerated(EnumType.STRING)
+    private Ruolo ruolo;
+
+    public Utente(String username, String mail, String password, String nome, String cognome, Ruolo ruolo) {
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.ruolo = ruolo;
+    }
+
+}
