cf53b8c108f4744d004a12e743febdff56e8d77b
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-23 16:42:37 +0200

    Update FatturaController.java

--- src/main/java/Epicode/epicenergy/controllers/FatturaController.java
+++ src/main/java/Epicode/epicenergy/controllers/FatturaController.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/fatture")
+@RequestMapping("/fatture")
 public class FatturaController {
 
     @Autowired
