da712407d2067c1c718428d8829a723f63b69940
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-23 12:54:13 +0200

    classi payloads Cliente

--- src/main/java/Epicode/epicenergy/repositories/ClienteRepository.java
+++ src/main/java/Epicode/epicenergy/repositories/ClienteRepository.java
@@ -2,7 +2,9 @@ package Epicode.epicenergy.repositories;
 
 import Epicode.epicenergy.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
 
